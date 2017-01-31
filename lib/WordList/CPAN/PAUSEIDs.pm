c8cc34d4f2c1f14e416bf120dc0f0b18c1288da4
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-31 11:23:05 +0700

    Initial commit

---
+++ lib/WordList/CPAN/PAUSEIDs.pm
@@ -0,0 +1,17 @@
+package WordList::CPAN::PAUSEIDs;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: CPAN PAUSE ID's
+
+=cut
+
+__DATA__
+# COMMAND: lcpan authors
